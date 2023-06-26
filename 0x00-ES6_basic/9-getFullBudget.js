da14f5e69cbe44dd485d20e7d5ef1b98c7f8efc0
sammyALX <samuelhailu49@gmail.com>
2023-06-26 23:08:54 +0300

    use ES6 method properties to simplify code

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
