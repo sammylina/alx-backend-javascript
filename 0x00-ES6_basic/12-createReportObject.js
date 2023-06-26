dd686600427ab0f898bd1ff2e3567c97b679f7d1
sammyALX <samuelhailu49@gmail.com>
2023-06-27 00:16:51 +0300

    directly return the parameter value

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,8 @@
 #!/usr/bin/env node
 
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const dep in employeesList) {
-    if (Object.hasOwn(employeesList, dep)) {
-      allEmployees[dep] = [...employeesList[dep]];
-    }
-  }
-
   return {
-    allEmployees,
+    allEmployees: employeesList,
     getNumberOfDepartments() {
       return Object.keys(employeesList).length;
     },
