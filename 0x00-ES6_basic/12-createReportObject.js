31c01be7de6a8509214876dd5e438bf34a1ef6eb
sammyALX <samuelhailu49@gmail.com>
2023-06-27 00:09:37 +0300

    create nested object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const dep in employeesList) {
+    if (Object.hasOwn(employeesList, dep)) {
+      allEmployees[dep] = employeesList[dep];
+    }
+  }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
