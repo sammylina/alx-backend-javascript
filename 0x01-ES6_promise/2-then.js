e42f81c21894336e26fa275481286ad80a358bb8
SammyAlx <samuelhailu49@gmail.com>
2023-06-28 09:15:18 +0300

    remove parenthesis from return object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => (Error('')))
+    .catch(() => new Error(''))
     .finally(() => {
       console.log('Got a response from the API');
     });
