be832546cb42df8dc3a6952b2723a05662cb116b
SammyAlx <samuelhailu49@gmail.com>
2023-06-28 17:23:27 +0300

    create response object after all promises are settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const arr = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      results.forEach((result) => {
+        const resObj = {
+          status: result.status,
+          value: result.value || result.reason.toString(),
+        };
+        arr.push(resObj);
+      });
+      return arr;
+    });
+}
