c2b0cdb900fe779766b7e3d230fb5228c4aaa487
SammyAlx <samuelhailu49@gmail.com>
2023-06-28 09:08:28 +0300

    add finally clause for logging message

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (Error('')))
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
