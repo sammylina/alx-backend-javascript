cc7ace6fc8363fe2c4b38fac2ac16153c0756b6e
sammyALX <samuelhailu49@gmail.com>
2023-06-29 20:37:53 +0300

    create simple ES6 class constructor

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
