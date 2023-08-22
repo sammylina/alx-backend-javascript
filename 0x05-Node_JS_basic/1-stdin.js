5be593b9ac57c602316f46a5c03285b17a1372b0
sammyALX <samuelhailu49@gmail.com>
2023-08-22 09:36:13 +0300

    convert readable stream data toString

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ console.log('Welcome to Holberton School, What is your name?');
 process.stdin.on('readable', () => {
   const data = process.stdin.read();
   if (data !== null) {
-    process.stdout.write(`Your name is: ${data}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
   }
   // process.exit(0)
 });
