3db8b1c673a045c896dfbe50bb9e8e9b4cedab9b
sammyALX <samuelhailu49@gmail.com>
2023-08-22 09:23:09 +0300

    fix eslint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 
-const {spawn, exec} = require('child_process')
-
-
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, What is your name?\n')
-process.stdin.on('readable', function() {
-  const data = process.stdin.read()
+process.stdout.write('Welcome to Holberton School, What is your name?\n');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
   if (data) {
-    process.stdout.write(`Your name is: ${data}` )
+    process.stdout.write(`Your name is: ${data}`);
   }
-  //process.exit(0)
-})
+  // process.exit(0)
+});
 
-process.stdin.on('end', function() {
-  process.stdout.write("This important software is now closing\n")
-})
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
