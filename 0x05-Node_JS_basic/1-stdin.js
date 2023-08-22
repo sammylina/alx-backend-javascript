95f9f93f971c283ad5fd0c400573dca750950d39
sammyALX <samuelhailu49@gmail.com>
2023-08-22 09:29:41 +0300

    remove unecessary newline chars

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,15 +2,15 @@
 
 process.stdin.setEncoding('utf-8');
 
-console.log('Welcome to Holberton School, What is your name?\n');
+console.log('Welcome to Holberton School, What is your name?');
 process.stdin.on('readable', () => {
   const data = process.stdin.read();
   if (data) {
-    console.log(`Your name is: ${data}`);
+    process.stdout.write(`Your name is: ${data}`);
   }
   // process.exit(0)
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
