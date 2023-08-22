ded673b2ec4abced871298363193eba6c0402b25
sammyALX <samuelhailu49@gmail.com>
2023-08-22 09:38:36 +0300

    convert console.log to stdout.write in stdin.end event

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -12,5 +12,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
