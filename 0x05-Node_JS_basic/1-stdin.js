c1785bfbc200682d1b79a0ce050e6ebdafd02c3a
sammyALX <samuelhailu49@gmail.com>
2023-08-22 09:25:06 +0300

    replace stdout.write with console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,15 +2,15 @@
 
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, What is your name?\n');
+console.log('Welcome to Holberton School, What is your name?\n');
 process.stdin.on('readable', () => {
   const data = process.stdin.read();
   if (data) {
-    process.stdout.write(`Your name is: ${data}`);
+    console.log(`Your name is: ${data}`);
   }
   // process.exit(0)
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing\n');
 });
