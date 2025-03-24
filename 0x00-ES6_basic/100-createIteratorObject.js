9d0e83f80938b73344ef225ac43ee98b1558b12d
sammyALX <samuelhailu49@gmail.com>
2025-03-24 09:13:44 +0300

    fix lint issue

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,12 @@
 export default function* createIteratorObject(report) {
-    const emp = report.allEmployees;
-    for (const idx in emp) {
-        for (const name in emp[idx]) {
-            yield emp[idx][name];
+  const emp = report.allEmployees;
+  for (const idx in emp) {
+    if (Object.hasOwn(emp, idx)) {
+      for (const name in emp[idx]) {
+        if (Object.hasOwn(emp[idx], name)) {
+          yield emp[idx][name];
         }
+      }
     }
+  }
 }
